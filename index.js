c9f3b204c52bf16200583f43dc254e8180f60825
Sandeep Kumar <skumar131431@gmail.com>
2025-07-26 15:00:58 +0530

    back2

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express();
+require('dotenv').config();
+const main =  require('./src/config/db')
+const cookieParser =  require('cookie-parser');
+const redisClient = require('./src/config/redis');
+const problemRouter = require('./src/routes/ContestProblem')
+const contestRouter = require('./src/routes/Contest');
+// const submitRouter = require("./routes/submit")
+const cors = require('cors')
+
+
+
+app.use(cors({
+    origin: 'https://code-ace-frontend.vercel.app',
+    credentials: true 
+}))
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/problem',problemRouter);
+app.use('/contest',contestRouter);
+
+
+
+
+
+
+const InitalizeConnection = async ()=>{
+    try{
+
+        await Promise.all([main(),redisClient.connect()]);
+        console.log("DB Connected");
+        
+        app.listen(process.env.PORT, ()=>{
+            console.log("Server listening at port number: "+ process.env.PORT);
+        })
+
+    }
+    catch(err){
+        console.log("Error: "+err);
+    }
+}
+
+
+InitalizeConnection();
+//redisClient.connect()
+
+
+
